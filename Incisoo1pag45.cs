d1948ef3b2d3c366418fd2ced10b006bf2e50080
26VAE01 <ajcholonvianka16db@gmail.com>
2026-05-09 23:51:35 -0600

    Add files via upload

---
+++ Incisoo1pag45.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace while_no_se_cumpla
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int monedas = 0;
+
+            Console.WriteLine("Ingrese la cantidad de monedas:");
+            monedas = Convert.ToInt32(Console.ReadLine());
+
+            while (monedas < 350)
+            {
+                Console.WriteLine("Nivel 5 bloqueado");
+
+                Console.WriteLine("Ingrese nuevamente la cantidad de monedas:");
+                monedas = Convert.ToInt32(Console.ReadLine());
+            }
+
+            Console.WriteLine("Nivel 5 desbloqueado");
+        }
+    }
+}
